56fe21762b70788465d21cf953b6488b66128f90
Kostiantyn Smyrnov <kostysh@gmail.com>
2021-05-21 03:08:41 +0300

    fixed release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,5 +8,5 @@ curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: Bearer $1" \
-  https://api.github.com/repos/windingtree/org.id-tools/dispatches \
+  https://api.github.com/repos/windingtree/org.id-sdk/dispatches \
   -d "{\"event_type\":\"release_to_npm\", \"client_payload\": {\"packageName\": \"$2\"}}"
