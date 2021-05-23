e631ebb04d8c6ef5ef681a06ea7c25fad764265d
Kostiantyn Smyrnov <kostysh@gmail.com>
2021-05-24 00:01:15 +0300

    disable linting error in package test-helpers

--- packages/test-helpers/src/orgIdSetup.ts
+++ packages/test-helpers/src/orgIdSetup.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const contract = require('@truffle/contract');
 import Web3 from 'web3';
 import { OrgIdContract } from '@windingtree/org.id';
